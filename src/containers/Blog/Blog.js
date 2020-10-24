bf8823e2a4b3938b2f397a9d833f4340b4fe1999
Istvan Halasz <ihalasz@pentalog.com>
2020-10-24 21:13:01 +0300

    added routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import axios from '../../axios';
+import { Route, Link } from 'react-router-dom';
+
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 
 import './Blog.css';
@@ -13,12 +15,18 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
-                <Posts />
+                {/*<Route path="/" exact render={() => <h1>Home</h1>} />*/}
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post" component={NewPost} />
 
             </div>
         );
