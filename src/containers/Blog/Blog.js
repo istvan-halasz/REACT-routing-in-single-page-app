0d31b494ca7e805ec4dbe9a12cc8778a1120d2eb
Istvan Halasz <ihalasz@pentalog.com>
2020-10-25 21:20:04 +0200

    loading Routes lazily

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 // import axios from 'axios';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
-import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
-
-
 import './Blog.css';
+import Posts from './Posts/Posts';
+import asyncComponent from '../../hoc/asyncComponent';
+//import NewPost from './NewPost/NewPost';
+
+const AsyncNewPost = asyncComponent( () =>{
+    return import('./NewPost/NewPost');
+} )
+
 
 class Blog extends Component {
     state = {
@@ -38,7 +42,7 @@ class Blog extends Component {
                 </header>
                 {/*<Route path="/" exact render={() => <h1>Home</h1>} />*/}
                 <Switch>
-                    {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
                     <Route path="/posts" component={Posts} />
                     <Route render={() => <h1>Not found</h1>} />{/*catching not defined links*/ }
                     {/*<Redirect from="/" to="/posts" />*/}
