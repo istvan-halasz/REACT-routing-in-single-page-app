11d70ec73844e2178df3eef7075a06e2c4c4bce6
Istvan Halasz <ihalasz@pentalog.com>
2020-10-25 20:49:43 +0200

    implementing navigation

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
@@ -9,6 +9,11 @@ import NewPost from './NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        auth: true
+    }
+
+
     render() {
         return (
             <div className="Blog">
@@ -33,8 +38,11 @@ class Blog extends Component {
                 </header>
                 {/*<Route path="/" exact render={() => <h1>Home</h1>} />*/}
                 <Switch>
-                    <Route path="/new-post" component={NewPost} />
+                    {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
                     <Route path="/posts" component={Posts} />
+                    <Route render={() => <h1>Not found</h1>} />{/*catching not defined links*/ }
+                    {/*<Redirect from="/" to="/posts" />*/}
+                    {/*<Route path="/" component={Posts} />*/}
                 </Switch>
             </div >
         );
